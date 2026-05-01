d955ffbb867eeb59b44b3df91c0547640b6fa647
Mai Ly Le <mai.ly.le@student.auc.nl>
2026-05-01 11:51:00 +0200

    fixed bug in arbitrary causing disconnected sc

--- src/SMCDEL/Simplicial/S5.hs
+++ src/SMCDEL/Simplicial/S5.hs
@@ -80,10 +80,11 @@ getLocalVar (SMS5 _ _ val) vert = case M.lookup vert val of
 getGlobalVar :: SimplicialModelS5 -> Facet -> [Prp]
 getGlobalVar sm = concatMap (getLocalVar sm)
 
+-- | Get the agent colouring the given vertex in the given model
 agAt :: SimplicialModelS5 -> Vert -> Agent
 agAt (SMS5 _ col _) v = col M.! v
 
--- | Get a list of all neighbouring facets where all given agents sit at an intersection  
+-- | Get a list of all neighbouring facets where all given agents sit at an intersection
 getRelFacets :: SimplicialModelS5 -> Facet -> [Agent] -> [Facet]
 getRelFacets (SMS5 sc col _) facet ags = filter (\x -> (ags `intersect` map (col M.!) (facet `intersect` x)) == ags) sc
 
@@ -156,10 +157,12 @@ instance Arbitrary SimplicialModelS5 where
     arbitrary = do
         let nonActualVerts = [6..30]
             verts = [1..30]
+        -- colour verts [6..30] randomly
         nonActualCol <- M.fromList <$> mapM (\v -> do
             ag <- elements defaultAgents
             return (v, ag)
             ) nonActualVerts
+        -- colour verts [1..5] with respective agent to ensure feasibility of facets
         let col = M.insert 1 "1" $
                   M.insert 2 "2" $
                   M.insert 3 "3" $
@@ -172,52 +175,14 @@ instance Arbitrary SimplicialModelS5 where
             ) verts
         let containsAllAg facet = all (\ag -> ag `elem` map (col M.!) facet) defaultAgents
         let facet = sort <$> vectorOf 5 (elements verts) `suchThat` containsAllAg
-            connected sc = all (\f1 -> any (\f2 -> f1 `intersect` f2 /= []) (sc \\ [f1])) sc
-        sc <- nub <$> resize 9 (listOf1 facet) `suchThat` connected
+            connected sc = 
+                length sc == 1 || 
+                all (\f1 -> any (\f2 -> f1 `intersect` f2 /= []) (delete f1 sc)) sc
+        sc <- (nub <$> resize 9 (listOf1 facet)) `suchThat` connected
         return $ SMS5 sc col val
-
-
--- instance Arbitrary SimplicialModelS5 where
---     arbitrary = do
---         let verts = [1..100 :: Vert]
---         -- let col = M.fromList $ zip verts (concat $ replicate 9 defaultAgents)
---         col <- M.fromList <$> mapM (\v -> do
---             ag <- elements defaultAgents
---             return (v, ag)
---             ) verts
---         let containsAllAg facet = all (\ag -> ag `elem` map (col M.!) facet) defaultAgents
---         initFacet <- vectorOf 5 (elements verts) `suchThat` containsAllAg
---         -- let initFacet = [1, 2, 3, 4, 5]
---         size <- chooseInt (1, 9)
---         let fix f = x where x = f x
---         sc <- fix (\f sc -> do
---             connectTo <- elements sc
---             newFacetPart <- sublistOf connectTo `suchThat` (\x -> length x < 5 && not (null x))
---             let agIn = map (col M.!)
---             newFacet <- fix (\g vs-> do
---                 newV <- elements verts `suchThat` (\v -> (col M.! v) `notElem` agIn vs)
---                 let newFace = newV : vs
---                 if length newFace < 5 then g newFace
---                                       else return newFace
---                 ) newFacetPart
---             let newSc = newFacet : sc
---             if any (newFacet `seteq`) sc 
---                 then f sc
---                 else if length newSc < size
---                         then f newSc
---                         else if size == 1 then return sc else return newSc
-            
---             ) [initFacet]
---         let usedVerts = vertsOf sc
---             colActual = M.filterWithKey (\k _ -> k `elem` usedVerts) col
---         val <- M.fromList <$> mapM (\v -> do
---             let prp = P $ read (col M.! v) - 1
---             ass <- M.singleton prp <$> choose (True, False)
---             return (v,ass)
---             ) usedVerts
---         return $ SMS5 sc colActual val
---     shrink sm@(SMS5 sc _ _) = 
---         [ sm `withoutFacet` x | x <- sc, not (null $ delete x sc) ]
+    -- shrink might break connectivity!
+    shrink sm@(SMS5 sc _ _) = 
+        [ sm `withoutFacet` x | x <- sc, not (null $ delete x sc) ]
 
 instance {-# OVERLAPPING #-} Arbitrary PointedSimplicialModelS5 where
     arbitrary = do
@@ -230,78 +195,3 @@ instance {-# OVERLAPPING #-} Arbitrary MultipointedSimplicialModelS5 where
         sm <- arbitrary :: Gen SimplicialModelS5
         xs <- sublistOf (facetsOf sm) `suchThat` (not . null)
         return (sm, xs)
-
--- Examples
-
--- Fig. 4 from proposal
-exampleSM :: SimplicialModelS5
-exampleSM = SMS5
-    [[1, 2, 3], [2, 3, 4]]
-    (M.fromList [(1, "a"), (2, "b"), (3, "c"), (4, "a")])
-    (M.fromList [(1, M.fromList [(P 1, True)]), (2, M.fromList [(P 2, True)]), (3, M.fromList [(P 3, False)]), (4, M.fromList [(P 1, False)])])
-
--- Fig. 4 from proposal, pointing towards facet X
-pointedExampleSM :: PointedSimplicialModelS5
-pointedExampleSM = (exampleSM, [1, 2, 3] :: Facet)
-
--- C' from Example 22 in [Dit+22] (p. 32)
-exampleSM2 :: SimplicialModelS5
-exampleSM2 = SMS5
-    [[1, 2, 3], [2, 3, 4], [3, 4, 5], [5, 6, 7], [6, 7, 8], [7, 8, 9]]
-    (M.fromList [(1, "a"), (2, "b"), (3, "c"), (4, "a"), (5, "b"), (6, "a"), (7, "c"), (8, "b"), (9, "a")])
-    (M.fromList [(1, M.fromList [(P 1, False)]), (2, M.fromList [(P 2, True)]), (3, M.fromList [(P 3, True)]), (4, M.fromList [(P 1, True)]), (5, M.fromList [(P 2, False)]), (6, M.fromList [(P 1, True)]), (7, M.fromList [(P 3, False)]), (8, M.fromList [(P 2, True)]), (9, M.fromList [(P 1, False)])])
-
-{-
-Some tests:
-
-In the simplicial model depicted in Fig. 4 in the proposal pointing towards 
-\(X\), it is true that \(a\) knows \(p_a\), since \(p_a\) is true in \(X\) and 
-only \(X\) is accessible for \(a\).
->>> pointedExampleSM |= (K "a" (PrpF (P 1)))
-True
-
-It is not true however, that \(b\) knows \(p_a\), since \(p_a\) is not true in 
-\(Y\) and the facets \(X\) and \(Y\) are indistinguishable for \(b\).
->>> pointedExampleSM |= (K "b" (PrpF (P 1)))
-False
-
-It is not true that the agents \(b\) and \(c\) distributedly know that 
-\(p_a \wedge p_b\), since \(p_a\) is true in \(X\) but not in \(Y\) and both
-\(X\) and \(Y\) are indistinguishable for both \(b\) and \(c\) (i.e. neither of 
-them knows that \(p_a\) is true).
->>> pointedExampleSM |= (Dk ["b", "c"] (conj (PrpF (P 1)) (PrpF (P 2))))
-False
-
-However, they distributedly know that \(p_b \wedge \neg p_c\), since this 
-formula holds in both \(X\) and \(Y\).
->>> pointedExampleSM |= (Dk ["b", "c"] (conj (PrpF (P 2)) (Neg (PrpF (P 3)))))
-True
-
-Furthermore, while \(b\) and \(c\) among each other do not, all agents 
-together distributedly know that \(p_a\) is true, since for \(a\), only \(X\) 
-(where \(p_a\) is true) is accessible.
-Distributed knowledge among all agents is trivial, since a formula is 
-distributedly known among all agents iff it is true in the current facet.
->>> pointedExampleSM |= (Dk ["a", "b", "c"] (PrpF (P 1)))
-True
-
-Initially, it is not true that \(p_a\) is common knowledge (both \(b\) and \(c\)
-are uncertain about \(p_a\) since \(X\) and \(Y\) are related for agents \(b\) 
-and \(c\)) and \(p_a\) is true in \(X\) but false in \(Y\)).
->>> eval pointedExampleSM (Ck ["a", "b", "c"] (PrpF (P 1)))
-False
-
-After publicly announcing that \(p_a\) is true, \(p_a\) is common knowledge 
-among all agents.
->>> eval pointedExampleSM (PubAnnounce (PrpF (P 1)) (Ck ["a", "b", "c"] (PrpF (P 1))))
-True
-
-In \( \mathcal{C}'\) from Ex. 22 in [Dit+22], pointing towards \(X\), it is not 
-common knowledge among all agents that \(p_c\) is true, since \(b\) cannot 
-distinguish between the left part of the simplicial complex, where \(p_c\) is 
-true, and the right part of the complex, where \(p_3\) is false (formally: 
-\(p_c\) is not true in \(V\), but \(X\) and \(V\) are
-connected via \( c\in \chi(X\cap Z)\) and \( b\in \chi(Z\cap V) \)). 
->>> eval (exampleSM2, [1, 2, 3]) (Ck ["a", "b", "c"] (PrpF (P 3)))
-False
--}
