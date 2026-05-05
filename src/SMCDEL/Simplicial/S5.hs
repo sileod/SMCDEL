befaa1702866ed70346fca430e8a4dc087f28d97
Mai Ly Le <mai.ly.le@student.auc.nl>
2026-05-06 00:17:23 +0200

    Simplicial Action Models prototype

--- src/SMCDEL/Simplicial/S5.hs
+++ src/SMCDEL/Simplicial/S5.hs
@@ -16,6 +16,7 @@ module SMCDEL.Simplicial.S5 where
 import Data.List
 import qualified Data.Map.Strict as M
 import Test.QuickCheck
+import Data.Maybe
 
 import SMCDEL.Language
 import SMCDEL.Internal.Help
@@ -67,14 +68,16 @@ type MultipointedSimplicialModelS5 = (SimplicialModelS5, [Facet])
 instance (HasFacets a, Pointed a b) => HasFacets (a, b) where
     facetsOf = facetsOf . fst
 
+class HasVertices a => HasColours a where
+    agAt :: a -> Vert -> Agent
+
+instance HasColours SimplicialModelS5 where
+    agAt (SMS5 _ verts) v = fst $ verts M.! v
+
 -- | Get a list of variables that are true in a given facet
 trueIn :: SimplicialModelS5 -> Facet -> [Prp]
 trueIn (SMS5 _ verts) = concatMap trueAt where
     trueAt v = (M.keys . M.filter id) (snd $ verts M.! v)
-
--- | Get the agent colouring the given vertex in the given model
-agAt :: SimplicialModelS5 -> Vert -> Agent
-agAt (SMS5 _ verts) v = fst $ verts M.! v
 
 -- | Get a list of all neighbouring facets where all given agents sit at an intersection
 getRelFacets :: SimplicialModelS5 -> Facet -> [Agent] -> [Facet]
@@ -118,7 +121,7 @@ eval pm (Kw ag form) = eval pm (K ag form) || eval pm (K ag (Neg form))
 eval pm (Ckw ag form) = eval pm (Ck ag form) || eval pm (Ck ag (Neg form))
 eval pm (Dkw ags form) = eval pm (Dk ags form) || eval pm (Dk ags (Neg form))
 eval (sm, _) (G form) = isTrue sm form
-eval pm (PubAnnounce form1 form2) = not (eval pm form1) || eval (update pm form1) form2 
+eval pm (PubAnnounce form1 form2) = not (eval pm form1) || eval (update pm form1) form2
 eval _ (Dia _ _) = undefined -- TODO
 
 instance Semantics SimplicialModelS5 where
@@ -129,6 +132,13 @@ instance Semantics PointedSimplicialModelS5 where
 
 instance Semantics MultipointedSimplicialModelS5 where
     isTrue (sm, xs) form = all (\x -> isTrue (sm, x) form) xs
+
+instance Pointed SimplicialModelS5 Vert
+
+-- local semantics
+instance Semantics (SimplicialModelS5, Vert) where
+    isTrue (sm, v) = isTrue (sm, xs) where
+        xs = filter (v `elem`) (facetsOf sm)
 
 instance Update SimplicialModelS5 Form where
     unsafeUpdate sm@(SMS5 sc verts) form = SMS5 newsc newverts where
@@ -167,14 +177,14 @@ instance Arbitrary SimplicialModelS5 where
         let vertMapAll = randomVertMap `M.union` semirandomVertMap
             containsAllAg x = all (\ag -> ag `elem` map (fst . (vertMapAll M.!)) x) defaultAgents
             facet = sort <$> vectorOf 5 (elements verts) `suchThat` containsAllAg
-            connected sc = 
-                length sc == 1 || 
+            connected sc =
+                length sc == 1 ||
                 all (\f1 -> any (\f2 -> f1 `intersect` f2 /= []) (delete f1 sc)) sc
         sc <- (nub <$> resize 9 (listOf1 facet)) `suchThat` connected
         let vertMap = M.filterWithKey (\k _ -> k `elem` vertsOf sc) vertMapAll
         return $ SMS5 sc vertMap
     -- shrink might break connectivity!
-    shrink sm@(SMS5 sc _) = 
+    shrink sm@(SMS5 sc _) =
         [ sm `withoutFacet` x | x <- sc, not (null $ delete x sc) ]
 
 instance {-# OVERLAPPING #-} Arbitrary PointedSimplicialModelS5 where
@@ -188,3 +198,72 @@ instance {-# OVERLAPPING #-} Arbitrary MultipointedSimplicialModelS5 where
         sm <- arbitrary :: Gen SimplicialModelS5
         xs <- sublistOf (facetsOf sm) `suchThat` (not . null)
         return (sm, xs)
+
+-- * Simplicial Action Models
+
+type PostCondition = M.Map Prp Form
+
+data SimplicialActionModelS5 = SActMS5
+    SimplicialComplex
+    (M.Map Vert (Agent, (Form, PostCondition)))
+    deriving (Eq, Ord, Show)
+
+instance HasFacets SimplicialActionModelS5 where
+    facetsOf (SActMS5 sc _) = sc
+
+instance HasVertices SimplicialActionModelS5 where
+    vertsOf (SActMS5 _ verts) = M.keys verts
+
+instance HasColours SimplicialActionModelS5 where
+    agAt (SActMS5 _ verts) v = fst $ verts M.! v
+
+instance HasPrecondition SimplicialActionModelS5 where
+    preOf _ = Top
+
+instance HasPrecondition (SimplicialActionModelS5, Vert) where
+    preOf (SActMS5 _ verts, v) = fst $ snd $ verts M.! v
+
+instance HasPrecondition (SimplicialActionModelS5, Facet) where
+    preOf (am, facet) = Conj $ map (\v -> preOf (am, v)) facet
+
+instance Update SimplicialModelS5 SimplicialActionModelS5 where
+    unsafeUpdate sm@(SMS5 _ vertMapSM) am@(SActMS5 _ vertMapAM) =  SMS5 sc newVertMap where
+        newFacetsInternal = [[(v, v') |
+                                v <- facetSM, v' <- facetAM, agAt sm v == agAt am v'] |
+                                   facetSM <- facetsOf sm,
+                                   facetAM <- facetsOf am,
+                                   (sm, facetSM) |= preOf (am, facetAM)]
+        newVertsInternal = nub $ concat newFacetsInternal
+        newVerts = take (length newVertsInternal) [(1 :: Vert)..]
+        intToAct vInt = fromJust (vInt `elemIndex` newVertsInternal) + 1
+        sc = map (map intToAct) newFacetsInternal
+        newVertMap = M.fromList $ map (\v -> (v, (agAt sm (vSM v), ass (vSM v) (vAM v)))) newVerts where
+            vSM v = fst $ newVertsInternal !! (v - 1)
+            vAM v = snd $ newVertsInternal !! (v - 1)
+        ass vSM vAM = M.fromList $ map (\p -> (p, isTrue (sm, vSM) (post vAM p))) (localPs vSM)
+        localPs vSM = M.keys $ snd $ vertMapSM M.! vSM
+        post vAM prp = snd (snd (vertMapAM M.! vAM)) M.! prp
+
+-- Example 28 in [Dit+22] (p. 39)
+ex28SM :: SimplicialModelS5
+ex28SM = SMS5 
+    [[1, 2, 3], [2, 3, 5], [2, 4, 5], [3, 5, 6]] 
+    (M.fromList [(1, ("c", M.singleton (P 3) False)), 
+                 (2, ("b", M.singleton (P 2) True)), 
+                 (3, ("a", M.singleton (P 1) True)), 
+                 (4, ("a", M.singleton (P 1) False)), 
+                 (5, ("c", M.singleton (P 3) True)), 
+                 (6, ("b", M.singleton (P 2) False))])
+
+ex28AM :: SimplicialActionModelS5
+ex28AM = SActMS5 
+    [[1, 2, 3], [2, 3, 4]] 
+    (M.fromList [(1, ("c", (Neg (PrpF (P 3)), M.singleton (P 3) (PrpF (P 3))))),
+                 (2, ("b", (Neg (disj (K "b" (PrpF (P 3))) (K "b" (Neg (PrpF (P 3))))), M.singleton (P 2) (PrpF (P 2))))),
+                 (3, ("a", (Neg (disj (K "a" (PrpF (P 3))) (K "a" (Neg (PrpF (P 3))))), M.singleton (P 1) (PrpF (P 1))))),
+                 (4, ("c", (PrpF (P 3), M.singleton (P 3) (PrpF (P 3)))))])
+
+{-
+>>> update ex28SM ex28AM
+SMS5 [[1,2,3],[2,3,4]] (fromList [(1,("c",fromList [(P 3,False)])),(2,("b",fromList [(P 2,True)])),(3,("a",fromList [(P 1,True)])),(4,("c",fromList [(P 3,True)]))])
+-}
